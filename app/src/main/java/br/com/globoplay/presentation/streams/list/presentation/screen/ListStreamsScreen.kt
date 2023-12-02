3a8229eb29a6d972f5947418de1296df44331752
Lucas Moro <lucas.rmoro1@gmail.com>
2023-12-02 14:12:57 -0300

    Using lazy column instead of scrollable column to Compose can recycle the column views

--- app/src/main/java/br/com/globoplay/presentation/streams/list/presentation/screen/ListStreamsScreen.kt
+++ app/src/main/java/br/com/globoplay/presentation/streams/list/presentation/screen/ListStreamsScreen.kt
@@ -1,19 +1,14 @@
 package br.com.globoplay.presentation.streams.list.presentation.screen
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -31,24 +26,16 @@ fun ListStreamsScreen(
     onNavigateToStreamDetails: (Int) -> Unit
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-    val scrollBehavior =
-        TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
-    val scrollState = rememberScrollState()
 
     LaunchedEffect(key1 = Unit) {
         viewModel.getGenreWithStreams()
     }
 
     CommonAppScreen(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         isScreenLoading = uiState.isLoading
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(scrollState),
-        ) {
-            uiState.genreWithStreams.forEach {
+        LazyColumn {
+            items(items = uiState.genreWithStreams) {
                 GenreWithStreamsCarousel(
                     content = it,
                     onNavigateToStreamDetails = onNavigateToStreamDetails,
